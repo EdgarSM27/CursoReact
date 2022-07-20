5ba12629a8b59243b07c8be966463bf9aff654ee
Edgar Sanchez <edgarsanchezmoreno1@gmail.com>
2022-07-19 18:11:04 -0600

    Se creó la carpeta components y se añadió el componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Fortune
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Galeria
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Productos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Informacion
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    
+     <NavBar />
+    
   );
 }
 
