c5eab89ec19c6d0ba929ad0102ea699120cc431f
Edgar Sanchez <edgarsanchezmoreno1@gmail.com>
2022-07-29 23:12:38 -0600

    Se corrigió el map utilizando props

--- src/components/ItemLIstContainer/ItemList.jsx
+++ src/components/ItemLIstContainer/ItemList.jsx
@@ -1,48 +1,19 @@
 import React from "react";
 import Item from "./Item";
 
-const ItemList = () => {
-  const productos = [
-    {
-      id: 1,
-      modelo: "Playera Just Do It",
-      talla: "XL",
-      Color: "Gris",
-    },
-    {
-      id: 2,
-      modelo: "Playera Adidas",
-      talla: "S",
-      Color: "Verde",
-    },
-    {
-      id: 3,
-      modelo: "Pans militares",
-      talla: "XXL",
-      Color: "Cafe",
-    },
-    {
-      id: 4,
-      modelo: "Gorra Red Bulls",
-      talla: "G",
-      Color: "Rojo",
-    },
-  ];
-  
-  return (
-    <div>
-{
-          productos.map((item) => (
-            <Item
-              key={item.id}
-              modelo={item.modelo}
-              talla={item.talla}
-              color={item.Color}
-            />
-          ))
-}
-    </div>
-  );
+const ItemList = (props) => {
+
+
+
+  return <div>{
+    props.productos.map((item) => (
+    <Item
+      key={item.id}
+      modelo={item.modelo}
+      talla={item.talla}
+      color={item.Color}
+    />
+  ))}</div>;
 };
 
 export default ItemList;
--- src/components/ItemLIstContainer/ItemListContainer.jsx
+++ src/components/ItemLIstContainer/ItemListContainer.jsx
@@ -2,9 +2,35 @@ import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 
 const ItemListContainer = (props) => {
+  const productos = [
+    {
+      id: 1,
+      modelo: "Playera Just Do It",
+      talla: "XL",
+      Color: "Gris",
+    },
+    {
+      id: 2,
+      modelo: "Playera Adidas",
+      talla: "S",
+      Color: "Verde",
+    },
+    {
+      id: 3,
+      modelo: "Pans militares",
+      talla: "XXL",
+      Color: "Cafe",
+    },
+    {
+      id: 4,
+      modelo: "Gorra Red Bulls",
+      talla: "G",
+      Color: "Rojo",
+    },
+  ];
   return (
     <div className="bg-gray-400">
-      <ItemList />
+      <ItemList productos={productos} />
     </div>
   );
 };
